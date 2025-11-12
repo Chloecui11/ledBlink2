2c6ec0232a111e950641ebec882d4e36c205b524
Chloecui11 <yz10800@nyu.edu>
2025-11-11 22:26:31 -0500

    added a button

--- ledBlink2.ino
+++ ledBlink2.ino
@@ -1,18 +1,18 @@
 int ledPin = 13;
 int ledPin2 = 14;
+int buttonPin = 29; 
 
 void setup() {
   pinMode(ledPin, OUTPUT);
   pinMode(ledPin2, OUTPUT);
+  pinMode(buttonPin, INPUT);
 }
 
 void loop() {
   digitalWrite(ledPin, HIGH);  
-  delay(500);
   digitalWrite(ledPin, LOW);
   delay(500); 
   digitalWrite(ledPin2, HIGH);  
-  delay(500);
   digitalWrite(ledPin2, LOW);
   delay(500); 
 
